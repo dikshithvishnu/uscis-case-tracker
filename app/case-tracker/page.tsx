a0e661a97d5d47207155abd9d836e8c4f8fbe0f8
Dikshith <dikshithvishnu2@gmail.com>
2025-03-26 00:10:47 -0400

    added contribute button

--- app/case-tracker/page.tsx
+++ app/case-tracker/page.tsx
@@ -4,12 +4,12 @@ import { useEffect, useState } from "react"
 import { useRouter } from "next/navigation"
 import { Button } from "@/components/ui/button"
 import { LogOut } from "lucide-react"
+import { FiGithub } from "react-icons/fi";
 import { USCIS_TOKEN } from "@/lib/constants"
 import CaseForm from "../appComponents/CaseForm"
 import CaseStatus from "../appComponents/CaseStatus"
 import ErrorMessage from "../appComponents/ErrorMessage"
 import { handleFetchCaseStatus } from "@/lib/api"
-
 
 interface CaseStatusResponse {
   case_status?: {
@@ -34,25 +34,24 @@ export default function CaseTracker() {
   const [isLoading, setIsLoading] = useState(false)
 
   useEffect(() => {
-    if(typeof window !== 'undefined') {
-        const token = window.sessionStorage.getItem(USCIS_TOKEN);
-        if(token) {
-            setIsLoggedIn(true)
-        } else {
-            router.push("/") 
-        }
+    if (typeof window !== "undefined") {
+      const token = window.sessionStorage.getItem(USCIS_TOKEN)
+      if (token) {
+        setIsLoggedIn(true)
+      } else {
+        router.push("/")
+      }
     }
   }, [router])
 
   const handleLogout = () => {
-    sessionStorage.removeItem(USCIS_TOKEN) 
+    sessionStorage.removeItem(USCIS_TOKEN)
     router.push("/")
   }
 
   const handleCaseSubmit = async (caseNumber: string) => {
-
     try {
-        handleFetchCaseStatus(caseNumber, setIsLoading, setCaseData);
+      handleFetchCaseStatus(caseNumber, setIsLoading, setCaseData)
     } catch (error) {
       console.error("Error fetching case status:", error)
       setIsLoading(false)
@@ -60,7 +59,7 @@ export default function CaseTracker() {
   }
 
   if (!isLoggedIn) {
-    return null 
+    return null
   }
 
   return (
@@ -78,14 +77,19 @@ export default function CaseTracker() {
 
         {caseData && !isLoading && (
           <div className="mt-8">
-            {caseData?.case_status && (
-                <CaseStatus caseData={caseData.case_status} />
-            )}
-            {!caseData?.case_status && (
-              <ErrorMessage message={caseData.message} />
-            )}
+            {caseData?.case_status && <CaseStatus caseData={caseData.case_status} />}
+            {!caseData?.case_status && <ErrorMessage message={caseData.message} />}
           </div>
         )}
+
+        <div className="mt-12 text-center">
+          <a href="https://github.com/dikshithvishnu/uscis-case-tracker" target="_blank">
+            <Button variant="outline" className="flex items-center gap-2">
+              <FiGithub className="h-4 w-4" />
+              Contribute on GitHub
+            </Button>
+          </a>
+        </div>
       </div>
     </main>
   )
